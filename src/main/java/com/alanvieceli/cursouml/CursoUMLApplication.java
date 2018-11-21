e929f035a90af2b64bb45f5c884f0578ec5e8412
Alan Vieceli <alansvieceli@gmail.com>
2018-11-21 21:43:58 -0200

    Iniciando Projeto

---
+++ src/main/java/com/alanvieceli/cursouml/CursoUMLApplication.java
@@ -0,0 +1,12 @@
+package com.alanvieceli.cursouml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoUMLApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoUMLApplication.class, args);
+	}
+}
