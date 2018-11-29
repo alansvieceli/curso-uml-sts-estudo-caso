50f3155ff99f6d5bdaa257eac02fea60c5af7398
Alan Vieceli <alansvieceli@gmail.com>
2018-11-29 20:16:18 -0200

    Criando operação de instanciação

--- src/main/java/com/alanvieceli/cursouml/CursoUMLApplication.java
+++ src/main/java/com/alanvieceli/cursouml/CursoUMLApplication.java
@@ -1,12 +1,32 @@
 package com.alanvieceli.cursouml;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.alanvieceli.cursouml.domains.Categoria;
+import com.alanvieceli.cursouml.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursoUMLApplication {
+public class CursoUMLApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoUMLApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		//Iniciando o banco de dados
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2)); 
+		
 	}
 }
