ac73a80481319f6063be2d6c0c78e6a71c484721
Alan Vieceli <alansvieceli@gmail.com>
2018-12-05 21:55:02 -0200

    Estado e cidade
    OneToMany
    ManyToOne

---
+++ src/main/java/com/alanvieceli/cursouml/domains/Estado.java
@@ -0,0 +1,95 @@
+package com.alanvieceli.cursouml.domains;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Estado  implements Serializable {
+
+	private static final long serialVersionUID = -3826541768724344659L;
+	
+	@Id  
+	@GeneratedValue(strategy=GenerationType.IDENTITY) 
+	private Integer id;
+	private String nome;
+	private String uf;
+	
+	@OneToMany(mappedBy="estado") //um para muitos (qual atribudo q mapeou, lá dentro da classe Cidade)
+	private List<Cidade> cidades = new ArrayList<Cidade>();
+	
+	public Estado() {
+		
+	}
+	
+	public Estado(Integer id, String nome, String uf) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.uf = uf;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public List<Cidade> getCidades() {
+		return cidades;
+	}
+
+	public void setCidades(List<Cidade> cidades) {
+		this.cidades = cidades;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Estado other = (Estado) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}	
+
+}
