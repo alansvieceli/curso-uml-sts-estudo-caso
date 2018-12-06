02b4ecbd1c086a5695715228d6b678f9d0030bd7
Alan Vieceli <alansvieceli@gmail.com>
2018-12-05 22:51:05 -0200

    Cliente, endereço e TipoCliente
    Tabela TELEFONE

--- src/main/java/com/alanvieceli/cursouml/CursoUMLApplication.java
+++ src/main/java/com/alanvieceli/cursouml/CursoUMLApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.alanvieceli.cursouml.domains.Categoria;
 import com.alanvieceli.cursouml.domains.Cidade;
+import com.alanvieceli.cursouml.domains.Cliente;
+import com.alanvieceli.cursouml.domains.Endereco;
 import com.alanvieceli.cursouml.domains.Estado;
 import com.alanvieceli.cursouml.domains.Produto;
+import com.alanvieceli.cursouml.domains.enums.TipoCliente;
 import com.alanvieceli.cursouml.repositories.CategoriaRepository;
 import com.alanvieceli.cursouml.repositories.CidadeRepository;
+import com.alanvieceli.cursouml.repositories.ClienteRepository;
+import com.alanvieceli.cursouml.repositories.EnderecoRepository;
 import com.alanvieceli.cursouml.repositories.EstadoRepository;
 import com.alanvieceli.cursouml.repositories.ProdutoRepository;
 
@@ -31,6 +36,12 @@ public class CursoUMLApplication implements CommandLineRunner{
 	@Autowired
 	private CidadeRepository cidadeRepository;
 	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoUMLApplication.class, args);
@@ -39,6 +50,8 @@ public class CursoUMLApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		//Iniciando o banco de dados
+		
+		//****************
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
@@ -56,7 +69,7 @@ public class CursoUMLApplication implements CommandLineRunner{
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2)); 
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
-		//
+		//****************
 		Estado est1 = new Estado(null, "Minas Gerais", "MG");
 		Estado est2 = new Estado(null, "São Paulo", "SP");
 		
@@ -69,6 +82,23 @@ public class CursoUMLApplication implements CommandLineRunner{
 		
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		//**************
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "98930710035", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("33871385", "991884406"));
+		
+		Cliente cli2 = new Cliente(null, "João da Luz", "joao@gmail.com", "00100597563", TipoCliente.PESSOAFISICA);
+		cli2.getTelefones().addAll(Arrays.asList("33485398", "92191306"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "91170481", cli1, c1);
+		Endereco e2 = new Endereco(null, "Av. Matos", "105", "Sala 33", "Centro", "80566000", cli1, c2);
+		Endereco e3 = new Endereco(null, "Rua das Margaridas", "30", "CASA", "Canas", "98741360", cli2, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));		
 		
 	}
 }
